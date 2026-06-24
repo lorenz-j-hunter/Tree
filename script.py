a340bb46851e2aef11f1f53e924e49a4095116ef
LorenzJHunter <lhunter1@iwu.edu>
2026-06-24 12:05:22 -0500

    wip

--- script.py
+++ script.py
@@ -13,22 +13,27 @@ def separate():
   print_no_newline('\n\n-------------------------------------------------\n\n')
 
 def clo(bf: int, h: int) -> int:
+  if h < 0:
+    raise RuntimeError('h < 0')
   ret: int = 0
   for ch in range(h):
     ret += pow(bf, ch)
   return ret
 
 def cap(bf: int, h: int) -> int:
+  if h < 0:
+    raise RuntimeError('h < 0')
   ret: int = 0
   for ch in range(h+1):
     ret += pow(bf, ch)
   return ret
 
 def height(bf: int, index: int) -> int:
+  if index < 0:
+    raise RuntimeError('index < 0')
   ret: int = 1
   if index == 0:
      return 0
-  
   while not (clo(bf, ret) <= index and index < cap(bf, ret)):
     ret += 1
   
@@ -46,28 +51,41 @@ def see(bf: int, blw: int, cur: int) -> bool:
   clo_cur = clo(bf=bf, h=h_cur)
   # case 1
   if h_blw is not h_cur+1:
-    print(f'First case failed')
+    print(f'\tsee(bf={bf}, blw={blw}, cur={cur}) returns False')
+    print(f'\tFirst case failed')
     return False
   r_blw: float = (blw - clo_blw) / pow(bf, h_blw)
   r_cur: float = (cur - clo_cur) / pow(bf, h_cur)
-  begin = math.floor((cur - clo_cur) / pow(bf, h_cur-1)) * pow(bf, h_cur) # begin
+  begin = math.floor(r_cur / (1 / pow(bf, h_cur))) * bf
   end = begin + bf # end of range
   r_begin = begin / pow(bf, h_blw)
   r_end = end / pow(bf, h_blw)
   # case 2
   if r_end == 1:
     if not (r_blw <= r_end and r_blw >= r_begin):
-      print(f'Second case failed.\n\nr_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\nend={end}\tr_begin={r_begin}\tr_end={r_end}')
+      print(f'\tsee(bf={bf}, blw={blw}, cur={cur}) returns False')
+      print(f'''
+            Second case failed.\n\n
+            r_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\n
+            end={end}\tr_begin={r_begin}\tr_end={r_end}
+            ''')
       return False
   elif r_begin == 0:
     if not (r_blw < r_end and r_blw >= r_begin):
-      print(f'Second case failed.\n\nr_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\nend={end}\tr_begin={r_begin}\tr_end={r_end}')
+      print(f'\tsee(bf={bf}, blw={blw}, cur={cur}) returns False')
+      print(f'''Second case failed.\n\n
+            r_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\n
+            end={end}\tr_begin={r_begin}\tr_end={r_end}''')
       return False
   else:
     if not (r_blw < r_end and r_blw >= r_begin):
-      print(f'Second case failed.\n\nr_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\nend={end}\tr_begin={r_begin}\tr_end={r_end}')
+      print(f'\tsee(bf={bf}, blw={blw}, cur={cur}) returns False')
+      print(f'''Second case failed.\n\n
+            r_blw={r_blw}\tr_cur={r_cur}\tbegin={begin}\n\n
+            end={end}\tr_begin={r_begin}\tr_end={r_end}''')
       return False
-  return True 
+  print(f'\tsee(bf={bf}, blw={blw}, cur={cur}) returns True')
+  return True
 
 def DFS(d_abs_ind=5):
   """Run all tests."""
@@ -78,58 +96,87 @@ def DFS(d_abs_ind=5):
   print_no_newline('d_abs_ind=', d_abs_ind, '\n')
   print_no_newline('d_abs_ind - clo(dh)=',d_abs_ind-cap_less_one)
   end_statement()
-  blw: int = -1
   future: int = -1
   offset: int = 0
   clo_: int = 0 
-  cap_: int = 1 
+  blw: int = -1
+  cap_: int = 0 
+  cur: int = 0
   begin_statement()
   for ch in range(1, dh+1):
-    cap_ += pow(bf, ch)
     if blw == -1:
       print(f'ch={ch}')
-      clo_ += pow(bf, ch-1)
+      clo_ = 1 
+      cap_ = 1 + bf
       # base case: future == -1
 
-      d: float = d_abs_ind - cap_less_one
-      frame: int = pow(bf, dh-1)
-      offst: int = math.floor(d / frame) * frame
+      """Create valid traversal indices."""
+      # create cur
+      cur = math.floor( ((d_abs_ind - cap_less_one) / pow(bf, dh)) / (1 / bf) ) + 1
+      r_cur: float = (cur - clo_ + 1) / pow(bf, ch)
+      near_begin = math.floor(r_cur / (1 / pow(bf, ch))) * bf # begin of range
+      near_end = near_begin + bf
 
-      # determine addition
-      r = (d_abs_ind - cap_less_one - offst) / frame
-      addition: int = math.floor(r / (1 / bf))
+      # traverse to cur here.
+      blw = 0
 
-      # create blw
-      sst: int = pow(addition*frame, (ch/dh))
-      blw: int = clo_ + sst + addition
-      print(f'trav_index={(blw-1) % bf}')
+      print(f'''
+            cur={cur}\t
+            ''')
       if ch == dh:
         break
-      future = offst
-      print(f'd={d}\tframe={frame}\tblw={blw}\n\nfuture={future}\tclo_={clo_}\toffst={offst}\n\naddition={addition}\tr={r}')
+      # create offst, future
+      d: float = d_abs_ind - cap_less_one
+      frame: int = pow(bf, dh-1)
+      offset = math.floor(d / frame) * frame
+      future = offset
+
+      clo_ = 0
+      cap_ = 1 
       separate()
+
     else:
+      # ch is currently that for blw, not cur.
       # recursive: future > 0
-      cap_ += pow(bf, ch) # same lvl as blw, currently.
-      clo_ += pow(bf, ch-1)
+      cap_ += pow(bf, ch-1) # same lvl as cur, currently.
+      clo_ += pow(bf, ch-2) # same lvl as cur, currently.
       print(f'ch={ch}')
 
-      # find frame
-      offset += future
-      frame: int = pow(bf, (dh-ch))
-      end = offset + pow(bf, (dh-ch+1))
+      """Find valid traversal index range."""
+      # create valid traversal range.
+      r_cur: float = (cur - clo_) / pow(bf, ch) # near
+      near_begin = math.floor(r_cur * pow(bf, ch)) * bf # begin of range
+      near_end = near_begin + bf # end of valid range
 
-      # create addition
-      r: float = (d_abs_ind - cap_less_one - offset) / (end - offset) # bottom
-      addition: int = math.floor(r / (1/bf))
+      """Hone in."""
+      # find frame
+      frame: int = pow(bf, (dh-ch+1)) # far
+      end = offset + frame  # far
+
+      # hone in with a ratio.
+      r = (d_abs_ind - cap_less_one - offset) / (end - offset) # far
+      addition: int = math.floor(r * bf) # near
+
+      """Select a valid index"""
+      # valid index is 'addition'.
+      blw = cap_ + near_begin + addition # near
 
       # create blw, trav_index
-      sst: int = math.floor(pow(offset, ch/dh))
-      blw: int = int(clo_ + sst + addition)
-      print(f'trav_index={(blw-1) % bf}')
-      print(f'offset={offset}\tframe={frame}\tr={r}\n\n1/bf={1/bf}\taddition={addition}\tsst={sst}\n\nblw={blw}\tclo_={clo_}')
+
+      print(f'''
+            blw={blw}\ttrav_index={(blw-1) % bf}\tcur={cur}\n\n
+            clo_={clo_}\tr={r}\toffset={offset}\n\n
+            end={end}\tnear_begin={near_begin}\tcap_={cap_}\n\n
+            addition={addition}
+            ''')
+      val: bool = see(bf=bf, blw=blw, cur=cur)
+
       separate()
-      future = addition * pow(bf, (dh-ch))
+
+      # future 
+      cur = blw
+      future = addition * pow(bf, (dh-ch)) # far
+      offset += future # far
   end_statement()
 
 def main():
