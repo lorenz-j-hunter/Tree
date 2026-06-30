0e75b57c89c2217a9e753b04ad3ee8d7dc714f07
LorenzJHunter <lhunter1@iwu.edu>
2026-06-30 05:11:06 -0500

    fix merge bugs.

--- Tree.cpp
+++ Tree.cpp
@@ -178,47 +178,6 @@ void Tree::insert(double data) {
       // valid index is 'addition'.
       if (ch == dh) {
         blw = cap_ + offset + addition; // near
-
-			} else {
-				abv = n_ssts + (cap_less_one_ - 1);
-
-			}
-      // create offst, future
-      int d = d_abs_ind - cap_less_one_;
-      int frame = pow(bf, dh-1);
-      offset = std::floor(d / frame) * frame;
-      future = offset;
-
-      clo_ = 0;
-      cap_ = 1;
-
-		} else {
-      // ch is currently that for blw, not cur.
-      // recursive: future > 0
-      cap_ += pow(bf, ch-1); // same lvl as cur, currently.
-      clo_ += pow(bf, ch-2); // same lvl as cur, currently.
-
-      /*Find valid traversal index range.*/
-      // create valid traversal range.
-      double r_cur = (cur - clo_) / std::pow(bf, ch); // near
-      double near_begin_d = std::floor(r_cur / (1 / std::pow(bf, ch))) * bf; // begin of range
-      int near_begin = static_cast<int>(near_begin_d); 
-      int near_end = near_begin + bf; // end of valid range
-
-      /*Hone in.*/
-      // find frame
-      int frame = std::pow(bf, (dh-ch+1)); // far
-      int end = offset + frame; // far
-
-      // hone in with a ratio.
-			double num = d_abs_ind - cap_less_one_ - offset;
-      double r = num / (end - offset); // far
-      int addition = std::floor(r * bf); // near
-
-      /*Select a valid index*/
-      // valid index is 'addition'.
-      if (ch == dh) {
-        blw = cap_ + offset + addition; // near
 			} else {
         blw = cap_ + near_begin + addition; // near
 			}
@@ -517,48 +476,6 @@ void Tree::insert(double data, int desired_depth, int desired_d) {
       // valid index is 'addition'.
       if (ch == dh) {
         blw = cap_ + offset + addition; // near
-
-			} else {
-				abv = n_ssts + (cap_less_one_ - 1);
-
-			}
-      // create offst, future
-      float d = d_abs_ind - cap_less_one_;
-      int frame = pow(bf, dh-1);
-      offset = floor(d / frame) * frame;
-      future = offset;
-
-      clo_ = 0;
-      cap_ = 1;
-
-		} else {
-      // ch is currently that for blw, not cur.
-      // recursive: future > 0
-      cap_ += pow(bf, ch-1); // same lvl as cur, currently.
-      clo_ += pow(bf, ch-2); // same lvl as cur, currently.
-
-      /*Find valid traversal index range.*/
-      // create valid traversal range.
-			double cur_num = cur - clo_;
-      double r_cur = cur_num / std::pow(bf, ch); // near
-      int near_begin = std::floor(r_cur * std::pow(bf, ch)) * bf; // begin of range
-      int near_end = near_begin + bf; // end of valid range
-
-
-      /*Hone in.*/
-      // find frame
-      int frame = std::pow(bf, (dh-ch+1)); // far
-      int end = offset + frame; // far
-
-      // hone in with a ratio.
-			double num = d_abs_ind - cap_less_one_ - offset;
-      double r = num / (end - offset); // far
-      int addition = std::floor(r * bf); // near
-
-      /*Select a valid index*/
-      // valid index is 'addition'.
-      if (ch == dh) {
-        blw = cap_ + offset + addition; // near
 			} else {
         blw = cap_ + near_begin + addition; // near
 			}
@@ -788,49 +705,6 @@ void Tree::insert(void* blank) {
 
       if (ch == dh) {
 				break;
-			}
-      // create offst, future
-      float d = d_abs_ind - cap_less_one_;
-      int frame = pow(bf, dh-1);
-      offset = floor(d / frame) * frame;
-      future = offset;
-
-      clo_ = 0;
-      cap_ = 1;
-
-		} else {
-      // ch is currently that for blw, not cur.
-      // recursive: future > 0
-      cap_ += pow(bf, ch-1); // same lvl as cur, currently.
-      clo_ += pow(bf, ch-2); // same lvl as cur, currently.
-
-			double bf_d = static_cast<double>(bf);
-
-      /*Find valid traversal index range.*/
-      // create valid traversal range.
-			double num_cur = cur - clo_;
-      double r_cur = num_cur / std::pow(bf, ch); // near
-      int near_begin = std::floor(r_cur * std::pow(bf, ch)) * bf; // begin of range
-      int near_end = near_begin + bf; // end of valid range
-
-      /*Hone in.*/
-      // find frame
-      int frame = std::pow(bf_d, (dh-ch+1)); // far
-      int end = offset + frame; // far
-
-      // hone in with a ratio.
-			double num = d_abs_ind - cap_less_one_ - offset;
-      double r = num / (end - offset); // far
-      int addition = std::floor(r * bf); // near
-
-      /*Select a valid index*/
-      // valid index is 'addition'.
-      if (ch == dh) {
-        blw = cap_ + offset + addition; // near
-
-			} else {
-				abv = n_ssts + (cap_less_one_ - 1);
-
 			}
       // create offst, future
       float d = d_abs_ind - cap_less_one_;
