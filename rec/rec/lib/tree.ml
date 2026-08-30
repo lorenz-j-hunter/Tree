2e1f214958471a1f1a53838aee4168a0f0bd2d41
LorenzJHunter <lhunter1@iwu.edu>
2026-08-30 16:25:37 -0500

    remove old, ocaml

--- rec/rec/lib/tree.ml
+++ rec/rec/lib/tree.ml
@@ -1,12 +1,21 @@
+open Stack
+
 type 'a tree =
   | Leaf
   | Node of 'a * 'a tree list
+
+let rec fill = fun l bf f ->
+  match l with
+  | [] -> l @ [f] 
+  | hd :: tl ->
+    if List.length tl = bf - 1 then
+      l @ [f]
+    else
+      fill l bf f 
 
 let rec preorder = fun (t : 'a tree) ->
   match t with
   | Leaf -> () 
   | Node (v, sts) -> 
-    let bf = List.length sts in
-      for i = 0 to bf - 1 do
-        preorder (List.nth sts i)
-      done
+    let l = fill [] (List.length sts) preorder in
+      ignore (l)
