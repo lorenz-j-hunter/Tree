cfe08ae1f0286c6de58c0d785c8989ce256975c7
LorenzJHunter <lhunter1@iwu.edu>
2026-09-01 15:37:14 -0500

    preorder

--- rec/test/test_tree.ml
+++ rec/test/test_tree.ml
@@ -1,3 +1,14 @@
 open Tree
+open Stack
 
-let () = print_endline "test_tree: FAILURE."
+let test_preorder () =
+  Alcotest.(check bool) "preorder doesn't crash" true (try
+    let s = ref (new stack) in
+    let r = root_ 1 in ignore (preorder r s); true
+    with _ -> false)
+
+let () =
+  let open Alcotest in
+  run "Tree" [
+    "preorder", [test_case "doesn't crash" `Quick test_preorder];
+  ]
